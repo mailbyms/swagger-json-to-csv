cc0ddd37b93909f8106d1643bdf618e41549819f
jianguoyong <jianguoyong@dm-ai.cn>
2023-11-09 09:56:26 +0800

    更改为遍历当前文件夹所有 .json 文件，把它们都转换为 .csv 文件

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # swagger-json-to-csv
-该工具适用于swagger生成的页面中对应的api-docs。  
-将api-docs的json内容导入到项目根目录下的1.txt，运行后结果导出为result.csv
+把 swagger 导出的 json 接口转换为 csv 格式（方便用 excel 编辑）  
+- 将 swagger 导出的 json 格式接口文件放到项目根目录下
+- 运行本项目，会生成所有 json 文件对应的 csv 文档
+- 可以再使用 `cat *.csv > all.csv` 把所有 csv 合并成一个大文件
--- src/main/java/com/chunmiao/jsontotxt/JsonToTxtApplication.java
+++ src/main/java/com/chunmiao/jsontotxt/JsonToTxtApplication.java
@@ -3,18 +3,63 @@ package com.chunmiao.jsontotxt;
 import com.chunmiao.jsontotxt.entity.Root;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.util.*;
 
 @SpringBootApplication
-public class JsonToTxtApplication {
+public class JsonToTxtApplication implements CommandLineRunner {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(JsonToTxtApplication.class, args);
-        FileInputStream in = new FileInputStream("1.txt");
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // 获取当前程序所在文件夹的路径
+        String folderPath = System.getProperty("user.dir");
+
+        // 创建一个表示文件夹的File对象
+        File folder = new File(folderPath);
+
+        // 获取文件夹中所有文件的列表
+        File[] files = folder.listFiles();
+
+        // 遍历文件列表
+        if (files != null) {
+            for (File file : files) {
+                String fileName = file.getAbsolutePath();
+                // 判断文件是否是以 .json 结尾
+                if (file.isFile() && fileName.endsWith(".json")) {
+                    // 打印完整文件名
+                    System.out.println(fileName);
+
+                    /**
+                     * 获得name的顺序，并按顺序写入csv
+                     */
+                    // 提取文件名的后缀
+                    int extensionIndex = fileName.lastIndexOf(".");
+                    String tempName = fileName.substring(0, extensionIndex);
+                    // 将文件名后缀改为 .csv
+                    String csvFileName = tempName + ".csv";
+                    // 打印完整文件名
+                    System.out.println(file.getAbsolutePath().replace(fileName, csvFileName));
+
+
+                    json2csv(fileName, csvFileName);
+                }
+            }
+        }
+
+    }
+
+    private static void json2csv(String srcFilename, String dstFilename) throws IOException {
+        FileInputStream in = new FileInputStream(srcFilename);
         JsonNode jsonNode = new ObjectMapper().readTree(in);
 
         /**
@@ -31,7 +76,7 @@ public class JsonToTxtApplication {
                 String method = methodsname.next(); //方法
                 JsonNode methods = tags.findValue(method);
                 String name = methods.findValue("tags").get(0).asText();
-                String description = methods.findValue("description").asText();
+                String description = methods.findValue("summary").asText();
 
                 Root root = new Root(name, method, api,description);  //当前查询到的一个接口数据
                 //放到hashmap里管理
@@ -52,9 +97,9 @@ public class JsonToTxtApplication {
         /**
          * 获得name的顺序，并按顺序写入csv
          */
-        File file = new File("result.csv");
+        File file = new File(dstFilename);
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(file), "GBK"));    //excel不能读取utf-8编码的csv文件
+                Files.newOutputStream(file.toPath()), "GBK"));    //excel不能读取utf-8编码的csv文件
 
         Iterator<JsonNode> names = jsonNode.findValue("tags").iterator();
         while (names.hasNext()) {
@@ -69,8 +114,8 @@ public class JsonToTxtApplication {
                     isFirst = false;
                 }
                 Root next = iterator1.next();
-                bufferedWriter.write(next.getMethod() + "," +
-                        next.getApi() + "," + next.getDescription());
+                bufferedWriter.write(next.getDescription() + "," +
+                        next.getApi());
                 bufferedWriter.newLine();
             }
 
@@ -88,21 +133,7 @@ public class JsonToTxtApplication {
 //                        + api);
 //                bufferedWriter.newLine();
 //        bufferedWriter.close();
-        Runtime.getRuntime().exec("cmd /c start F:/Project/JsonSoup/result.csv");
-        System.out.println("done");
-
-
+        // Runtime.getRuntime().exec("cmd /c start result.csv");
+        //System.out.println("done");
     }
-
-
-    public static Root jsontoentity(FileInputStream in) throws IOException {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        Root root = objectMapper.readValue(in, Root.class);
-
-        return root;
-
-
-    }
-
 }
